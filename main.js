558e0f746adf4bdeca59f96d76f003c8fac467e4
BrunoOS93 <103432986+BrunoOS93@users.noreply.github.com>
2022-11-10 01:12:28 -0300

    Retirada do delay

--- main.js
+++ main.js
@@ -8,10 +8,8 @@ function createGame(player1, hour, player2) {
   `
 }
 
-let delay = -0.1;
 function createCard(date, day, games) {
-  delay = delay + 0.1;
-  return `
+   return `
   <div class="card" style="animation-delay: ${delay}s">
                 <h2>${date} <span>${day}</span></h2>
                 <ul>
